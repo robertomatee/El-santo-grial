a2bf532ac166b4fabb4d70c30ef6cf9dc80ca84b
robertomatee <121132587+robertomatee@users.noreply.github.com>
2024-02-11 15:23:30 -0300

    config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 
 PASSWORD = 'abcd'
-API_KEY = '1JRt1mKuWxLYuXigdSccYxVpKkPMxpv8vhl33NKRoEJKXX07UbzQhKpiKtUBnJyP' 
-API_SECRET = 'c6CjTqhzn4yRLN2RfGGQIEB1O7nI9LR4Ub9lufLhseAJMMp8Sdtv0exjLYpf5JzQ'
+API_KEY = 'puto que buscas aca?' 
+API_SECRET = 'y aca que buscas salame?'
